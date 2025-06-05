354776d830edbb1eda97669f2a8190c2100d22db
Nvann07 <nrizwardi@gmail.com>
2025-06-05 17:29:27 +0700

    Menambahkan repersentasi single linked list class Stack

--- stack.cpp
+++ stack.cpp
@@ -15,4 +15,24 @@ class Node
   {
    next = NULL;
   }
+};
+
+//Stack class 
+class Stack
+{
+  private:
+  Node *top; // Pointer to the top node of the stack
+
+  public:
+  Stack()
+  {
+     top = NULL; // Initialize the stack with a null top 
+  }
+
+  //Push operation: Insert an element onto the top of the 
+  int push(int value)
+  {
+    
+  }
+  
 }
